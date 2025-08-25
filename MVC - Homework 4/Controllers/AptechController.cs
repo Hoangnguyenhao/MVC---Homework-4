9200f78bad34675666d9e73ee342f699499473fe
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-08-25 10:48:30 +0700

    Add files via upload

---
+++ MVC - Homework 4/Controllers/AptechController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class AptechController : System.Web.Mvc.Controller
+    {
+        public System.Web.Mvc.ActionResult Index()
+        {
+            ViewBag.Address = "285 Doi Can, Ba Dinh, Hanoi";
+            return View();
+        }
+
+        public ActionResult StudentList()
+        {
+            var students = new List<string> { "John Smith", "Anna Johnson", "David Lee" };
+            return View(students);
+        }
+
+        public ActionResult StudentDetail(int id)
+        {
+            var student = $"Student {id}: Sample Name";
+            return View((object)student);
+        }
+    }
+}
---
+++ MVC - Homework 4/Controllers/VietnamController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class VietnamController : Controller
+    {
+        public ActionResult Cities()
+        {
+            var cities = new List<string> { "Hanoi", "Ho Chi Minh City", "Da Nang", "Hue" };
+            return View(cities);
+        }
+    }
+}
