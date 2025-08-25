07fac94d8840a9340fa1659f3bafd6e514086e23
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-08-25 11:03:43 +0700

    Add files via upload

---
+++ MVC - Homework 4/Controllers/VietnamController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class VietnamController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Info = "Vietnam is located in Southeast Asia. People are friendly, geography is diverse from mountains to sea.";
+            return View();
+        }
+
+        public IActionResult ThanhPho()
+        {
+            var cities = new List<string>
+            {
+                "Ha Noi",
+                "Ho Chi Minh",
+                "Da Nang",
+                "Hue"
+            };
+            return View(cities);
+        }
+    }
+}
